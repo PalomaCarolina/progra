3910787b96c585da455b093be179bcc6c579e0af
PalomaCarolina <166861713+PalomaCarolina@users.noreply.github.com>
2024-04-19 08:17:38 -0600

    Update pininosc-1.c

--- pininosc-1.c
+++ pininosc-1.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
-void main() {
-int n, ne = 0;
-float edad, sumaEdad =0;
-float peso, sumaPeso = 0, mediaEdad = 0, mediaPeso = 0;
-printf("Introducir cantidad de estudiantes a procesar: ");
-scanf("%d",&n);
-while (ne<n) {
-ne++;
-printf("Introducir EDAD del estudiante %d: ",ne);
-scanf("%f",&edad);
-printf("Introducir PESO del estudiante %d: ",ne);
-scanf("%f",&peso);
-sumaEdad = sumaEdad + edad;
-sumaPeso = sumaPeso + peso;
-}
-mediaEdad = sumaEdad/n;
-mediaPeso = sumaPeso/n;
-printf("La edad promedio es %2.2f\n",mediaEdad);
-printf("El peso promedio es %2.2f\n", mediaPeso);
+#include <stdlib.h>
+#define MAX 100
+#define MIN 0
 
+struct preguntas {
+    char mascota[11];
+    char postre[50];
+    char mama[25];
+    float edad;
+};
+
+int main(){
+    struct preguntas contrasenia;
+    //struct estudiante grupoPE[MAX/2];
+    //int n, i;
+
+    printf ("\nIngresa nombre de tu mascota:");
+    scanf ("%s", contrasenia.mascota);
+    while(getchar()!='\n');
+    printf ("\nIngresa tu postre favorito: ");
+    scanf ("%[^\n]", contrasenia.postre);
+    while(getchar()!='\n');
+    printf ("\nProporciona el nombre de tu mama: ");
+    scanf ("%[^\n]", contrasenia.mama);
+    while(getchar()!='\n');
+    printf ("\nProporciona tu edad: ");
+    scanf ("%f", &contrasenia.edad);
+    while(getchar()!='\n');
+
+    printf("\nTu mascota es %s , tu postre favorito es %s, el nombre de tu mama es %s y actualmente tienes una edad de %2.2f\n", contrasenia.mascota, contrasenia.postre, contrasenia.mama, contrasenia.edad);
+
+
+    return 0;
 }
