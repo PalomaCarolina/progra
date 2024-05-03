8d34d0a003403ed0a20037ebfcdbc164a8afbe0d
caromarin <31226734+caromarin@users.noreply.github.com>
2024-05-03 07:59:29 -0600

    nuevo ejercicio
    Error:  File "/Users/Geme/Desktop/maestria/progra/ejercicio1.py", line 2, in <module>
        ["golden",11, "fresa",21, "dominico",31,]
    TypeError: list indices must be integers or slices, not tuple

---
+++ ejercicio1.py
@@ -0,0 +1,29 @@
+frutas=["manzana",1, "moras",2, "platano",3, 
+        ["golden",11, "fresa",21, "dominico",31,]
+        ["amarillo",12, "rojo",22, "amarilloynegro",32,]]
+for x in frutas:
+    if isinstance(x,list):
+        for y in x:
+            if isinstance(y,list):
+                for z in y:
+                    print (z)
+            else:
+                print (y)
+    else:
+        print (x)
+frutas=["manzana",1, "moras",2, "platano",3, 
+        ["golden",11, "fresa",21, "dominico",31,]
+        ["amarillo",12, "rojo",22, "amarilloynegro",32,]]
+def repeticion(frutas):
+    for x in frutas:
+        if isinstance(x,list):
+            for y in x:
+                if isinstance(y,list):
+                    for z in y:
+                        print (z)
+                else:
+                    print (y)
+        else:
+            print (x)
+                
+repeticion(frutas)
