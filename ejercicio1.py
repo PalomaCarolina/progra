fe0d8052f6e0838630ae286b2a5ae5de090e6a0b
caromarin <31226734+caromarin@users.noreply.github.com>
2024-05-03 08:09:50 -0600

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,6 +1,8 @@
 frutas=["manzana",1, "moras",2, "platano",3, 
-        ["golden",11, "fresa",21, "dominico",31,]
-        ["amarillo",12, "rojo",22, "amarilloynegro",32,]]
+        ["golden",11, "fresa",21, "dominico",31,
+        ["amarillo",12, "rojo",22, "amarilloynegro",32,]
+        ,"sangria",41, "chino",51]
+        ,"naranja",4, "Kiwi",5]
 for x in frutas:
     if isinstance(x,list):
         for y in x:
@@ -12,8 +14,10 @@ for x in frutas:
     else:
         print (x)
 frutas=["manzana",1, "moras",2, "platano",3, 
-        ["golden",11, "fresa",21, "dominico",31,]
-        ["amarillo",12, "rojo",22, "amarilloynegro",32,]]
+        ["golden",11, "fresa",21, "dominico",31,
+        ["amarillo",12, "rojo",22, "amarilloynegro",32,]
+        ,"sangria",41, "chino",51]
+        ,"naranja",4, "Kiwi",5]
 def repeticion(frutas):
     for x in frutas:
         if isinstance(x,list):
