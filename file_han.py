7d2dc8131d3d85a1e0e687c14350672a05ffbb52
PalomaCarolina <166861713+PalomaCarolina@users.noreply.github.com>
2024-05-17 12:34:19 -0600

    file_han.py

--- file_han.py
+++ file_han.py
@@ -1,1 +1,14 @@
-open(archivo.txt)
+try:
+    archivo=open("archivo.txt")
+    for linea in archivo:
+        try:
+            (personaje, dialogo)=linea.split(':')
+            print (linea, end="")
+            print(personaje, end='')
+            print(' dijo: ', end='')
+            print(dialogo, end="")
+        except:
+            pass
+    archivo.close()
+except:
+    print("el archivo no se encuentra")
