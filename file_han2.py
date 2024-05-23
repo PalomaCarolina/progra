78dcb16ec9cb5a9f61df774ca00201cb74a88c9d
caromarin <31226734+caromarin@users.noreply.github.com>
2024-05-23 11:20:49 -0600

    .

--- file_han2.py
+++ file_han2.py
@@ -16,3 +16,22 @@ try:
     archivo.close()
 except:
     print("el archivo no se encuentra")
+
+    try:
+    archivo=open("alumnos posgrados.txt")
+    for linea in archivo:
+        try:
+            (alumno, matricula, posgrado)=linea.split('-')
+            print (linea, end="")
+            print(' Alumno: ', end='')
+            print(alumno, end='')
+            print(' Matricula: ', end='')
+            print(matricula, end="")
+            print(' Posgrado: ', end='')
+            print(posgrado, end="")
+
+        except:
+            pass
+    archivo.close()
+except:
+    print("el archivo no se encuentra")
